13f13c5a8d03f7be2070b681222ef534c2f28e57
Kyosh1ro <57198664+Kyosh1ro@users.noreply.github.com>
2026-09-04 11:56:23 +0200

    feature : Labels added for buffs: +Dmg, Mdmg+... (#110)

--- lua/ky_localization.lua
+++ lua/ky_localization.lua
@@ -66,6 +66,15 @@ local function add_fallbacks(loc)
         ky_hud_score_best_streak = "BEST STREAK",
         ky_hud_score_best_short  = "BEST",
 
+        -- Libellés dessinés au-dessus de l'icône d'un buff
+        ky_hud_buff_label_inspire_cooldown = "Boost+",
+        ky_hud_buff_label_inspire_revive   = "Revive",
+        ky_hud_buff_label_damage_increase  = "Dmg+",
+        ky_hud_buff_label_damage_reduction = "Dmg-",
+        ky_hud_buff_label_melee_damage     = "M.Dmg+",
+        ky_hud_buff_label_health_regen     = "HP+",
+        ky_hud_buff_label_dodge_chance     = "Dodge",
+
         -- Bandeau des séries de kills
         ky_hud_combo_2     = "CLEAN PAIR",
         ky_hud_combo_2_2   = "DOUBLE TAP",
